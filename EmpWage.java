9a30f6b1dd9e5cd8a431587cf265a82ba236de0c
SumeetCodeJC <code.sumeet9@gmail.com>
2022-01-31 12:37:19 +0530

    Modified : Used case statement

--- EmpWage.java
+++ EmpWage.java
@@ -1,21 +1,24 @@
 package com.bridgelabz.empwage;
 
 public class EmpWage {
-	public static void main(String[] args){
-		int IS_PART_TIME =1;
-		int IS_FULL_TIME =2;
-		int WAGE_PER_HOUR =20;
-		int empHrs = 0;
-		int empWage = 0;
-		double empcheck = Math.floor(Math.random() * 10) %2;
-		if(empcheck == IS_FULL_TIME)
-			empHrs = 8;
-		else if(empcheck == IS_PART_TIME)
-			empHrs = 4;
-		else
-			empHrs = 0;
-		empWage = empHrs * WAGE_PER_HOUR;
-		System.out.println("Emp Wage: " + empWage);
-		}
+	public static final int IS_PART_TIME = 1;
+    public static final int IS_FULL_TIME = 2;
+    public static final int WAGE_PER_HOUR = 20;
+    public static void main(String[] args) {
+    	int empHrs = 0;
+        int empWage = 0;
+        int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+        switch (empCheck) {
+        case IS_PART_TIME :
+            empHrs = 4;
+            break;
+        case IS_FULL_TIME:
+            empHrs = 8;
+            break;
+        default:
+            empHrs = 0;
+        }
+        empWage = empHrs * WAGE_PER_HOUR;
+        System.out.println("Emp Wage:" +empWage);
+    }
 }
-
