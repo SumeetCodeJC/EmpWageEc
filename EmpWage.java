cd300d51e84a4aba5e2142186ad1b94623d21f20
SumeetCodeJC <code.sumeet9@gmail.com>
2022-01-31 12:23:49 +0530

    Modified : Emp wage calc

--- EmpWage.java
+++ EmpWage.java
@@ -2,15 +2,18 @@ package com.bridgelabz.empwage;
 
 public class EmpWage {
 	public static void main(String[] args) {
-		int IS_FULL_TIME =1;
+		int IS_FULL_TIME = 1;
+		int WAGE_PER_HOUR = 20;
+		int empHrs = 0;
+		int empWage = 0;
 		double empcheck = Math.floor(Math.random() * 10) %2;
 		if(empcheck == IS_FULL_TIME)
-		{
-			System.out.print("Employee is Present");
-		}
-		else {
-			System.out.print("Employee is Absent");
-		}
+			empHrs = 8;
+		else
+			empHrs = 0;
+		empWage = empHrs * WAGE_PER_HOUR;
+		System.out.println("Emp Wage: " + empWage);
+		
 	}
 
 }
